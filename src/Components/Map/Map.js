18dc7f2dc05d95db7fa40228003a45a94adbcfcb
Deron McGuire <Deronmcguire23@yahoo.com>
2021-12-01 18:59:51 -0600

    Added new component strcutures

---
+++ src/Components/Map/Map.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Map.css';
+
+import {Map} from  '../../Util/stores'
+
+
+function Map() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Map;
+
