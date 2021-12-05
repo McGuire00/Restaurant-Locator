0a1d2fbc2f9014f97c3d7119829afd7d689df176
Deron McGuire <Deronmcguire23@yahoo.com>
2021-12-04 21:33:33 -0600

    added map structure in index.html

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./Map.css";
 
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
 // import { Map } from "../../Util/stores";
 
 function Map() {
+  //   mapboxgl.accessToken = "YOUR_MAPBOX_ACCESS_TOKEN";
+
+  // mapboxgl.accessToken = "pk.eyJ1IjoiZGVyb25tYWMiLCJhIjoiY2tra2RydGRmMDhkaDJvcHB5bW45NmF3bSJ9.FtCWlzl41UQWKoIqIfE29w";
+  
+  // const map = new mapboxgl.Map({
+  //   container: "map", // container ID
+  //   style: "mapbox://styles/mapbox/streets-v11", // style URL
+  //   center: [-74.5, 40], // starting position [lng, lat]
+  //   zoom: 9, // starting zoom
+  // });
+
   return (
     <div>
       <div class="sidebar">
